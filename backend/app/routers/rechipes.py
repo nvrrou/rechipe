2c278c17369d9933514816f32967cf9b601a8dc0
yheanfernandez <yhean.fernandez@mail.udp.cl>
2026-05-16 19:21:28 -0400

    Implementacion de la obtencion de las caracteristicas nutricionales de los productos en la despensa mediante la IA

--- backend/app/routers/rechipes.py
+++ backend/app/routers/rechipes.py
@@ -1,12 +1,17 @@
 from fastapi import APIRouter
-from app.models.schemas import RecipeRequest
-from app.services.ai_service import generar_receta_con_ia
+from app.models.schemas import RecipeRequest, EsquemaAlimento
+from app.services.ai_service import generar_receta_con_ia, obtener_info_nutricional
 
 router = APIRouter(
     prefix="/recipes",
     tags=["Recetas"]
 )
 
+@router.post("/analizar-alimento")
+async def analizar_alimento(datos: EsquemaAlimento):
+    # Llamamos a la IA pasándole el nombre del producto
+    resultado = await obtener_info_nutricional(datos.nombre)
+    return resultado
 
 @router.post("/generar")
 async def generar_receta(request: RecipeRequest):
